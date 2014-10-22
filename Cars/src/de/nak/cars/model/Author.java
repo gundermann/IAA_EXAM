5dabc0a5809131cf888762cdc44ba9063c2e33cf
bob_sheknowdas <--->
2014-10-22 15:09:53 +0200

    cle:
    - Ergänzung zum letzten commit

---
+++ Cars/src/de/nak/cars/model/Author.java
@@ -0,0 +1,25 @@
+package de.nak.cars.model;
+
+public class Author {
+	/** The identifier. */
+	private Long keywordId;
+	/** The authors name. */
+	private String name;
+
+	public Long getKeywordId() {
+		return keywordId;
+	}
+
+	public void setKeywordId(Long keywordId) {
+		this.keywordId = keywordId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
