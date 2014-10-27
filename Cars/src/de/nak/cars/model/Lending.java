7158c8bc85faa3dcb49f2c29b6de48145de5ddc3
CaraFeelester <carafeelester@gmail.com>
2014-10-27 20:33:17 +0100

    Lending kennt seine Publication. (OneToOne Mapping)

--- Cars/src/de/nak/cars/model/Lending.java
+++ Cars/src/de/nak/cars/model/Lending.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
 
 /**
  * Lending entity.
@@ -17,12 +19,10 @@ import javax.persistence.Id;
 public class Lending {
 	/** The identifier. */
 	private Long lendingId;
-	// /** The lended publication. */
-	// private Publication publication;
+	/** The lended publication. */
+	private Publication publication;
 	/** The date the publication was lent */
 	private Date outgoDate;
-	// TODO: kann entfert werden, Lending kennt dann aber seinen eventuellen
-	// Admonition Process nicht!
 	// /** The person who lent the publication */
 	// private Lender lender;
 	/** Day when the publication should be returned. */
@@ -41,14 +41,6 @@ public class Lending {
 		this.lendingId = id;
 	}
 
-	// public Publication getPublication() {
-	// return publication;
-	// }
-	//
-	// public void setPublication(Publication publication) {
-	// this.publication = publication;
-	// }
-
 	public Date getOutgoDate() {
 		return outgoDate;
 	}
@@ -56,14 +48,6 @@ public class Lending {
 	public void setOutgoDate(Date outgoDate) {
 		this.outgoDate = outgoDate;
 	}
-
-	// public Lender getLender() {
-	// return lender;
-	// }
-	//
-	// public void setLender(Lender lender) {
-	// this.lender = lender;
-	// }
 
 	@Column(name = "return_date")
 	public Date getExpectedReturnDate() {
@@ -80,6 +64,16 @@ public class Lending {
 
 	public void setNumberOfLendingExtensions(Integer numberOfLendingExtensions) {
 		this.numberOfLendingExtensions = numberOfLendingExtensions;
+	}
+
+	@OneToOne
+	@PrimaryKeyJoinColumn
+	public Publication getPublication() {
+		return publication;
+	}
+
+	public void setPublication(Publication publication) {
+		this.publication = publication;
 	}
 
 }
